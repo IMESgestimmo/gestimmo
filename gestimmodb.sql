12168c4d723ca974c937baa8c50bdab8c7534a8c
Ouedraogo Kenneth <kennethbboy3@gmail.com>
2019-04-25 18:40:25 +0200

    Update DB

--- gestimmodb.sql
+++ gestimmodb.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `gestimmodb`
 --
-
+CREATE DATABASE `gestimmodb`;
+USE `gestimmodb`;
 -- --------------------------------------------------------
 
 --
